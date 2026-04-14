1001d9345ac4390ba474a9e1101f53d1e24c97f1
Vatcharamai <yoyo.rodring@gmail.com>
2026-04-15 06:06:15 +0700

    fix deployment 2

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
 import { createServer } from 'http';
 import { parse } from 'url';
 import next from 'next';
+import { createRequire } from 'module';
 
+const require = createRequire(import.meta.url);
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 const PORT = parseInt(process.env.PORT || '3000', 10);
 
-app.prepare().then(async () => {
+app.prepare().then(() => {
   const httpServer = createServer((req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
   });
 
   if (dev) {
-    const { register } = await import('ts-node');
-    register({ transpileOnly: true });
-    const { initSocket } = await import('./server/socket.ts');
+    require('ts-node').register({ transpileOnly: true });
+    const { initSocket } = require('./server/socket.ts');
     initSocket(httpServer);
   } else {
-    const { initSocket } = await import('./dist/server/socket.js');
+    const { initSocket } = require('./dist/server/socket.js');
     initSocket(httpServer);
   }
 
